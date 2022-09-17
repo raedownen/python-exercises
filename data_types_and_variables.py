86daf3cc215c56d9bd8612c7abbed00f38ff8a71
Rachel Downen <rae.l.downen@gmail.com>
2022-09-17 14:43:27 -0500

    corrections

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -6,25 +6,21 @@
 #0 int
 #True bool
 #'True' str
-#[{}] [{}]
-#{'a': []} invalid syntax
+#[{}] [{}] list
+#{'a': []} dict
 ###
 
 
 #2.What data type would best represent:
-
 #A term or phrase typed into a search box? string
-#If a user is logged in? string
+#If a user is logged in? bool
 #A discount amount to apply to a user's shopping cart? float
 #Whether or not a coupon code is valid? boolean
 #An email address typed into a registration form? string
-#The price of a product?float
-#A Matrix? int
-#The email addresses collected from distutils.fancy_getopt import fancy_getopt
-#from http.client import PAYMENT_REQUIRED
-#from xml.dom.pulldom import CHARACTERS
-#from a registration form? sting
-#Information about applicants to Codeup's data science program?string
+#The price of a product? float
+#A Matrix? list of list
+#The email addresses collected from a registration form? list
+#Information about applicants to Codeup's data science program? table
 
 
 #3. For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in your Python REPL.
@@ -55,37 +51,52 @@
 #Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet 
 #if they're going to like it). If price for a movie per day is 3 dollars, how much 
 #will you have to pay? daily_rental = 3, daily_rental * 3 * 5 * 1
-
+mer = 3
+bro = 5
+herc = 1
+price_rate = 3
+total_rate = (mer + herc + bro) * price_rate
 #5. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook,
 #they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, 
 # and Facebook 350. How much will you receive in payment for this week? You worked 10 
 #hours for Facebook, 6 hours for Google and 4 hours for Amazon.
-#hourly_rate * hours_worked weekly_pay
-#Google ghourly rate == 400, ghours_worked == 6, ghourly_rate * ghours_worked == gweekly_pay
+g_rate = 400
+am_rate =380
+fb_rate = 350
 
-#Amazon ahourly rate == 380, ahours_worked == 4, ahourly_rate * ahours_worked == aweekly_pay
+g_hours = 6
+am_hours = 4
+fb_hours = 10
 
-#Facebook fhourly rate == 350, fhours_worked == 10, fhourly_rate * fhours_worked == fweekly_pay
+g_pay = g_rate * g_hours
+am_pay = am_rate * am_hours
+fb_pay = fb_rate * fb_hours
 
-#gweekly_pay + aweekly_pay + fweekly_pay == gross_pay 
+paycheck = g_pay + am_pay + fb_pay
 
 #6. A student can be enrolled to a class only if the class is not full and the 
 #class schedule does not conflict with her current schedule.
- #enrollment == class_not_full and schedule_not_conflict
+class_has_space = True
+schedule_has_time = True
+enrollment = class_has_space and schedule_has_time
 
 #7. A product offer can be applied only if people buys more than 2 items, and the 
 #offer has not expired. Premium members do not need to buy a specific amount of products.
-#product_available == off not expired and product_amout > 2 or premium-member
+#product_offer = offer not expired and product_amout > 2 or premium-member
+offer_active = True
+premium_member = False
+more_than_two = True
+product_offer = (premium_member or more_than_two) and offer_active
 
 #8. Continue working in your data_types_and_variables.py file. Use the following code to follow the instructions below:
 #username = 'codeup'
 #password = 'notastrongpassword'
 #Create a variable that holds a boolean value for each of the following conditions:
 #the password must be at least 5 characters
-#password => 5 char
+check_one = len(my_password) >= 5 char
 #the username must be no more than 20 characters
-#username =< 20
+check_2 = my_username <= 20
 #the password must not be the same as the username
-#password != username
+check_3 = my_password != my_username
 #bonus neither the username or password can start or end with whitespace
-#username != ' ' and password != ' '
+check_4 = my_username != ' ' and my_password != ' '
